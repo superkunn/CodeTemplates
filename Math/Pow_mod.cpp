f1ea6e1ed72e6e8408801006309b2dc98b37d61c
superkunn <1150527505@qq.com>
2018-06-11 20:47:59 +0800

    add pow_mod.cpp

---
+++ Math/Pow_mod.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MOD=1e9+7;
+ll pow_mod(ll a,ll i){
+    ll ret=1;
+    while(i>0){
+        if(i&1)ret=(ret*a)%MOD;
+        a=(a*a)%MOD;
+        i>>=1;
+    }
+    return ret;
+}
+int main(){
+    cout<<pow_mod(2,4)<<endl;
+    return 0;
+}
