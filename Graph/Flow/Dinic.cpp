071420a6887caff13e9e7d69d3e090bb0c860440
superkunn <1150527505@qq.com>
2018-06-13 22:43:37 +0800

    add dinic

---
+++ Graph/Flow/Dinic.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MAXV=5000+5;
+const int MAXE=500000+5;
+const int INF=0x3f3f3f3f;
+int tot;
+int head[MAXV];
+int iter[MAXV];
+int level[MAXV];
+struct node{
+    int to;
+    int cap;
+    int rev;
+    int next;
+};
+node edge[2*MAXE];// two times!!!
+void add_edge(int a,int b,int c){
+    edge[tot].to=b;
+    edge[tot].cap=c;
+    edge[tot].rev=tot+1;
+    edge[tot].next=head[a];
+    head[a]=tot++;
+
+    edge[tot].to=a;
+    edge[tot].cap=0;
+    edge[tot].rev=tot-1;
+    edge[tot].next=head[b];
+    head[b]=tot++;
+}
+void bfs(int s){
+    memset(level,-1,sizeof(level));
+    queue<int> que;
+    level[s]=0;
+    que.push(s);
+    while(!que.empty()){
+        int v=que.front();que.pop();
+        for(int i=head[v];i!=-1;i=edge[i].next){
+            node &e=edge[i];
+            if(e.cap>0&&level[e.to]<0){
+                level[e.to]=level[v]+1;
+                que.push(e.to);
+            }
+        }
+    }
+}
+int dfs(int v,int t,int f){
+    if(v==t)return f;
+    for(int &i=iter[v];i!=-1;i=edge[i].next){
+        node &e = edge[i];
+        if(e.cap>0&&level[v]<level[e.to]){
+            int d=dfs(e.to,t,min(f,e.cap));
+            if(d>0){
+                e.cap-=d;
+                edge[e.rev].cap+=d;
+                return d;
+            }
+        }
+    }
+    return 0;
+}
+int max_flow(int s,int t){
+    int flow=0;
+    while(1){
+        bfs(s);
+        if(level[t]<0)return flow;
+        for(int i=0;i<MAXV;i++){
+            iter[i]=head[i];
+        }
+        int f;
+        while((f=dfs(s,t,INF))>0){
+            flow+=f;
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    tot=0;
+    memset(head,-1,sizeof(head));
+    for(int i=1;i<=n;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        add_edge(a,b,c);
+    }
+    int x,y;
+    cin>>x>>y;
+    cout<<max_flow(x,y)<<endl;
+    return 0;
+}
+/*
+int:
+7
+0 1 10
+1 2 6
+0 2 2
+1 3 6
+3 8 8
+2 8 5
+3 2 3
+0 8
+out:
+11
+*/
---
+++ Graph/MinimumSpanningTree/Kruskal.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXV=1e5+5;
+const int MAXE=1e5+5;
+const int INF=1e9;
+const int MAXN=1e5+5;
+struct edge{int u,v,cost;};
+bool cmp(const edge &e1,const edge &e2)
+{
+    return e1.cost<e2.cost;
+}
+edge es[MAXE];
+int V,E;
+int p[MAXN],r[MAXN];
+void init(int n){
+    for(int i=1;i<=n;i++){
+        p[i]=i;
+        r[i]=0;
+    }
+}
+int findp(int x){
+    if(x==p[x])return x;
+    else return p[x]=findp(p[x]);
+}
+void unite(int x,int y){
+    x=findp(x);
+    y=findp(y);
+    if(x==y)return;
+    if(r[x]<r[y]){
+        p[x]=y;
+    }
+    else{
+        p[y]=x;
+        if(r[x]==r[y])r[x]++;
+    }
+}
+bool same(int x,int y){
+    return findp(x)==findp(y);
+}
+int kruskal(){
+    sort(es,es+E,cmp);
+    init(V);
+    int res=0;
+    for(int i=0;i<E;i++){
+        edge e=es[i];
+        if(!same(e.u,e.v)){
+            unite(e.u,e.v);
+            res+=e.cost;
+        }
+    }
+    return res;
+}
+int main(){
+    return 0;
+}
