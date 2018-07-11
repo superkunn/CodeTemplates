d5d52e35ba359cb8daab6e993608f1255b8f5eb1
superkun <superkunn@users.noreply.github.com>
2018-07-11 17:43:44 +0800

    Update DSU.cpp

--- DataStruct/DSU.cpp
+++ DataStruct/DSU.cpp
@@ -1,46 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MAXN=1e6+5;
-int p[MAXN],r[MAXN];
-void init(int x){
-    for(int i=0;i<=x;i++){
-        p[i]=i;
-        r[i]=0;
+struct DSU{
+    int p[MAXN],r[MAXN];
+    void init(int x){
+        for(int i=0;i<=x;i++){
+            p[i]=i;
+            r[i]=0;
+        }
     }
-}
-int findp(int x){
-    if(x==p[x])return x;
-    else return p[x]=findp(p[x]);
-}
-void unite(int x,int y){
-    x=findp(x);
-    y=findp(y);
-    if(x==y)return;
-    if(r[x]<r[y]){
-        p[x]=y;
+    int findp(int x){
+        if(x==p[x])return x;
+        else return p[x]=findp(p[x]);
     }
-    else{
-        p[y]=x;
-        if(r[x]==r[y])r[x]++;
+    void unite(int x,int y){
+        x=findp(x);
+        y=findp(y);
+        if(x==y)return;
+        if(r[x]<r[y]){
+            p[x]=y;
+        }
+        else{
+            p[y]=x;
+            if(r[x]==r[y])r[x]++;
+        }
     }
-}
-bool same(int x,int y){
-    return findp(x)==findp(y);
-}
+    bool same(int x,int y){
+        return findp(x)==findp(y);
+    }    
+}dsu;
 int main(){
     int n;
     cin>>n;
-    init(n);
+    dsu.init(n);
     int m;
     cin>>m;
     while(m--){
         int o,x,y;
         cin>>o>>x>>y;
         if(o==1){
-            unite(x,y);
+            dsu.unite(x,y);
         }
         else{
-            cout<<(int)same(x,y)<<endl;
+            cout<<(int)dsu.same(x,y)<<endl;
         }
     }
     return 0;
