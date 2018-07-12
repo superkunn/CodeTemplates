e657660e1cf9509ba8e02d54789c8be008164fc4
superkun <superkunn@users.noreply.github.com>
2018-07-12 22:59:35 +0800

    Update Pow_mod.cpp

--- Math/Pow_mod.cpp
+++ Math/Pow_mod.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int MOD=1e9+7;
-ll pow_mod(ll a,ll i){
+ll pow_mod(ll a,ll i,ll mod){
     ll res=1;
     while(i>0){
-        if(i&1)res=res*a%MOD;
-        a=a*a%MOD;
+        if(i&1)res=res*a%mod;
+        a=a*a%mod;
         i>>=1;
     }
     return res;
 }
 int main(){
-    cout<<pow_mod(2,4)<<endl;
     return 0;
 }
