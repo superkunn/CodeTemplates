f3f271fa264e4bc75932e15a5785a1a087ef3ff7
superkun <superkunn@users.noreply.github.com>
2018-07-12 22:20:43 +0800

    Update SPFA.cpp

--- Graph/ShortestPath/SPFA.cpp
+++ Graph/ShortestPath/SPFA.cpp
@@ -1,22 +1,27 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<vector>
 using namespace std;
-const int MAXV=1e4+5;
-const int MAXE=1e6+5;
+const int MAXV=1e5+50;
 const int INF=1e9;
 struct edge{int to,cost;};
 typedef pair<int,int> P;
 int V;
 vector<edge> G[MAXV];
 int d[MAXV];
+int cnt[MAXV];
 bool inque[MAXV];
 queue<int> que;
-void spfa(int s){
-    fill(d,d+V,INF);
-    fill(inque,inque+V,false);
-    d[s]=0;
+int spfa(int s){
+    fill(d,d+V+1,INF);
+    fill(cnt,cnt+V+1,0);
+    fill(inque,inque+V+1,false);
     while(!que.empty()) que.pop();
     que.push(s);
     inque[s]=true;
+    d[s]=0;
+    cnt[s]=1;
     while(!que.empty()){
         int u=que.front();
         inque[u]=false;
@@ -26,13 +31,33 @@ void spfa(int s){
             if(d[u]+e.cost<d[e.to]){
                 d[e.to]=d[u]+e.cost;
                 if(!inque[e.to]){
+                    if(++cnt[e.to]>V)return -1;
                     inque[e.to]=true;
                     que.push(e.to);
                 }
             }
         }
     }
+    return d[V];
 }
 int main(){
+    int a1,a2,a3;
+    scanf("%d%d%d",&a1,&a2,&a3);
+    V=a1;
+    for(int i=1;i<=V;i++){
+        G[i].push_back(edge{i-1,0});
+    }
+    for(int i=1;i<=a2;i++){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        G[x].push_back(edge{y,z});
+    }
+    for(int i=1;i<=a3;i++){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        G[y].push_back(edge{x,-z});
+    }
+    int w=spfa(1);
+    printf("%d",w==INF?-2:w);
     return 0;
 }
