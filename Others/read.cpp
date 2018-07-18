6d32bd62107cb2263e20c97bf1b8aa8fe0d2f2af
superkun <superkunn@users.noreply.github.com>
2018-07-18 14:16:30 +0800

    Update read.cpp

--- Others/read.cpp
+++ Others/read.cpp
@@ -1,5 +1,4 @@
-inline ll read()
-{
+inline long long read(){
     int x=0,f=1;char ch=getchar();
     while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
     while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
