39473d2c51de50302bd138c81fb1b82a00bac76b
superkun <superkunn@users.noreply.github.com>
2018-07-18 14:40:32 +0800

    Update DSU.cpp

--- DataStruct/DSU.cpp
+++ DataStruct/DSU.cpp
@@ -5,11 +5,7 @@ struct DSU{
     int p[MAXN];
     void init(int n){for(int i=0;i<=n;i++)p[i]=i;}
     int findp(int x){return x==p[x]?x:p[x]=findp(p[x]);}
-    void unite(int x,int y){
-        x=findp(x),y=findp(y);
-        if(x==y)return;
-        p[y]=x;
-    }
+    void unite(int x,int y){x=findp(x);y=findp(y);if(x==y)return;p[y]=x;}
     bool same(int x,int y){return findp(x)==findp(y);}
 }dsu;
 int main(){
