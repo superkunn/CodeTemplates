86343100d5fdac04bd7028de4ab709d8fdb555d6
superkun <superkunn@users.noreply.github.com>
2018-07-18 15:08:42 +0800

    Update Kruskal.cpp

--- Graph/MinimumSpanningTree/Kruskal.cpp
+++ Graph/MinimumSpanningTree/Kruskal.cpp
@@ -5,32 +5,11 @@ using namespace std;
 const int MAXE=1e5+5;
 const int MAXN=1e5+5;
 struct DSU{
-    int p[MAXN],r[MAXN];
-    void init(int x){
-        for(int i=0;i<=x;i++){
-            p[i]=i;
-            r[i]=0;
-        }
-    }
-    int findp(int x){
-        if(x==p[x])return x;
-        else return p[x]=findp(p[x]);
-    }
-    void unite(int x,int y){
-        x=findp(x);
-        y=findp(y);
-        if(x==y)return;
-        if(r[x]<r[y]){
-            p[x]=y;
-        }
-        else{
-            p[y]=x;
-            if(r[x]==r[y])r[x]++;
-        }
-    }
-    bool same(int x,int y){
-        return findp(x)==findp(y);
-    }    
+    int p[MAXN];
+    void init(int n){for(int i=0;i<=n;i++)p[i]=i;}
+    int findp(int x){return x==p[x]?x:p[x]=findp(p[x]);}
+    void unite(int x,int y){x=findp(x);y=findp(y);if(x==y)return;p[y]=x;}
+    bool same(int x,int y){return findp(x)==findp(y);}
 }dsu;
 struct edge{int u,v,cost;}es[MAXE];
 bool cmp(const edge &e1,const edge &e2){
