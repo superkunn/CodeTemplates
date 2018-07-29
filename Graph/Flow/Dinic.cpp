f7af4aafbbb52152e38be1233803ce4ead6c2ee0
superkun <superkunn@users.noreply.github.com>
2018-07-29 15:53:18 +0800

    Update Dinic.cpp

--- Graph/Flow/Dinic.cpp
+++ Graph/Flow/Dinic.cpp
@@ -1,37 +1,30 @@
 #include<cstdio>
 #include<iostream>
-#include<vector>
 #include<queue>
 #include<cstring>
 using namespace std;
 const int MAXV=5000+5;
 const int MAXE=500000+5;
 const int INF=0x3f3f3f3f;
-namespace dinic{
 int tot;
-int head[MAXV];
-int iter[MAXV];
-int level[MAXV];
+int head[MAXV],level[MAXV],iter[MAXV];
 struct node{
-    int to,cap,rev,next;
-};
-node edge[2*MAXE];// two times!!!
+    int to,cap,next;
+}edge[MAXE<<1];
 void init(){
     tot=0;
     memset(head,-1,sizeof(head));
 }
-void add_edge(int a,int b,int c){
-    edge[tot].to=b;
+void add_edge(int u,int v,int c){
+    edge[tot].to=v;
     edge[tot].cap=c;
-    edge[tot].rev=tot+1;
-    edge[tot].next=head[a];
-    head[a]=tot++;
-
-    edge[tot].to=a;
+    edge[tot].next=head[u];
+    head[u]=tot++;
+    
+    edge[tot].to=u;
     edge[tot].cap=0;
-    edge[tot].rev=tot-1;
-    edge[tot].next=head[b];
-    head[b]=tot++;
+    edge[tot].next=head[v];
+    head[v]=tot++;
 }
 void bfs(int s){
     memset(level,-1,sizeof(level));
@@ -39,25 +32,28 @@ void bfs(int s){
     level[s]=0;
     que.push(s);
     while(!que.empty()){
-        int v=que.front();que.pop();
-        for(int i=head[v];i!=-1;i=edge[i].next){
-            node &e=edge[i];
-            if(e.cap>0&&level[e.to]<0){
-                level[e.to]=level[v]+1;
-                que.push(e.to);
+        int u=que.front();
+        que.pop();
+        for(int i=head[u];i!=-1;i=edge[i].next){
+            int v=edge[i].to;
+            int c=edge[i].cap;
+            if(c>0&&level[v]<0){
+                level[v]=level[u]+1;
+                que.push(v);
             }
         }
     }
 }
-int dfs(int v,int t,int f){
-    if(v==t)return f;
-    for(int &i=iter[v];i!=-1;i=edge[i].next){
-        node &e = edge[i];
-        if(e.cap>0&&level[v]<level[e.to]){
-            int d=dfs(e.to,t,min(f,e.cap));
+int dfs(int u,int t,int f){
+    if(u==t)return f;
+    for(int &i=iter[u];i!=-1;i=edge[i].next){
+        int v=edge[i].to;
+        int &c=edge[i].cap;
+        if(c>0&&level[u]<level[v]){
+            int d=dfs(v,t,min(f,c));
             if(d>0){
-                e.cap-=d;
-                edge[e.rev].cap+=d;
+                c-=d;
+                edge[i^1].cap+=d;
                 return d;
             }
         }
@@ -78,19 +74,18 @@ int max_flow(int s,int t){
         }
     }
 }
-};
 int main(){
     int n;
     cin>>n;
-    dinic::init();
+    init();
     for(int i=1;i<=n;i++){
         int a,b,c;
         cin>>a>>b>>c;
-        dinic::add_edge(a,b,c);
+        add_edge(a,b,c);
     }
     int x,y;
     cin>>x>>y;
-    cout<<dinic::max_flow(x,y)<<endl;
+    cout<<max_flow(x,y)<<endl;
     return 0;
 }
 /*
