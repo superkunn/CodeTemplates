a7ac1e7004b2529ff9ab4cc28e0e21c27b310448
superkun <superkunn@users.noreply.github.com>
2018-07-31 16:12:55 +0800

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -2,33 +2,32 @@
 #include<cstdio>
 #include<cstring>
 using namespace std;
-void kmp_pre(char x[],int xlen,int nxt[]){
-    int i,j;
-    j=nxt[0]=-1;
-    i=0;
-    while(i<xlen){
-        while(j!=-1&&x[i]!=x[j])j=nxt[j];
-        nxt[++i]=++j;
+const int MAXN=2e6;
+void kmp_pre(char p[],int plen,int nxt[]){
+    int i=0;
+    int j=nxt[0]=-1;
+    while(i<plen){
+        while(j!=-1&&p[i]!=p[j])j=nxt[j];
+        if(p[++i]==p[++j])nxt[i]=nxt[j];
+        else nxt[i]=j;
     }
 }
-int nxt[2000000];
-int kmp_count(char x[],int xlen,char y[],int ylen){
-    int i,j;
-    int ans=0;
-    kmp_pre(x,xlen,nxt);
-    i=j=0;
-    while(i<ylen){
-        while(-1!=j&&y[i]!=x[j])j=nxt[j];
+int nxt[MAXN];
+int kmp_count(char p[],int plen,char t[],int tlen){
+    int i=0,j=0,ans=0;
+    kmp_pre(p,plen,nxt);
+    while(i<tlen){
+        while(j!=-1&&t[i]!=p[j])j=nxt[j];
         i++;j++;
-        if(j>=xlen){
+        if(j>=plen){
             ans++;
-            j=nxt[j]; 
+            j=nxt[j];
         }
     }
     return ans;
 }
-char a[2000000];
-char b[2000000];
+char a[MAXN];
+char b[MAXN];
 int main() {
     int t;
     scanf("%d",&t);
